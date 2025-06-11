7c40f6149495baf3e20ab3d5e5dc615ddc59d5a7
Zuba-NSR-1040-15 <Zerubabeltemeselew@gmail.com>
2025-06-11 04:16:16 -0700

    Update mst_Kruskal's.cpp

--- mst_Kruskal's.cpp
+++ mst_Kruskal's.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-struct Edge
-{
-    int src,dest;
+
+struct Edge {
+    int src, dest;
     int weight;
+
+    bool operator<(const Edge& other) const {
+        return weight < other.weight;
+    }
 };
 
+// Represents a graph using a list of vertices and edges.
 struct Graph {
-    int v;
-    vector<Edge> edges; // list of edges
+    int V;
+    vector<Edge> edges;
 };
 
-void findMST (Graph& g){
-    cout <<"\nfindMST function called, but not yet implemented."<< endl;
+void kruskalMST(Graph& g) {
+    sort(g.edges.begin(), g.edges.end());
+    
+    cout << "\n--- Kruskal's Algorithm Execution ---\n";
+    cout << "Edges sorted by weight:\n";
+    for(const auto& edge : g.edges) {
+        cout << "Edge " << edge.src << "-" << edge.dest << " Weight: " << edge.weight << endl;
+    }
+
 }
 
-int main(){
-    cout<< "kruskal's Algorithm for mst defining structures"<<endl;
+int main() {
+    cout << "===== Kruskal's Algorithm - Sorting Step =====\n";
+    
     Graph graph;
-    graph.v=4;
+    graph.V = 4;
+    graph.edges.push_back({0, 1, 10});
+    graph.edges.push_back({1, 2, 2}); 
+    graph.edges.push_back({0, 2, 6});
+    graph.edges.push_back({2, 3, 4});
+    graph.edges.push_back({0, 3, 5});
 
-    graph.edges.push_back({0,1,10});
-    graph.edges.push_back({0,2,6});
-    graph.edges.push_back({0,3,5});
-
-    cout<< "Graph created with " << graph.v<<" vertices and " <<graph.edges.size()<<" edges."<<endl;
-    findMST(graph);
+    kruskalMST(graph);
+    
     return 0;
 }
