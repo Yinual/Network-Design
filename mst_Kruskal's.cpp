d66738b3b160f925bf1e25a17d1c72f5c138af0b
Ashenafi-117 <Ashenafiberihun80@gmail.com>
2025-06-11 14:30:49 +0300

    Update mst_Kruskal's.cpp

--- mst_Kruskal's.cpp
+++ mst_Kruskal's.cpp
@@ -7,62 +7,69 @@ struct Edge { int src, dest, weight;
     bool operator<(const Edge& o) const { return weight < o.weight; 
     } 
 };
-struct Graph { int V; vector<Edge> edges; 
-    Graph(int vertices) : V(vertices) {}
-};
 
-struct DSU {
+    struct DSU {
     vector<int> parent;
     DSU(int n) { parent.resize(n); 
-        for(int i=0; i<n; ++i) parent[i]=i; }
-
-    int find(int i) {
-        if (parent[i] == i) return i;
-        return find(parent[i]);
+        for(int i=0; i<n; ++i) parent[i]=i; 
     }
-    void unite(int i, int j) {
-        int root_i = find(i);
-        int root_j = find(j);
-        if(root_i != root_j) {
-            parent[root_i] = root_j;
-        }
+    int find(int i) { 
+        if (parent[i] == i) return i; 
+        return find(parent[i]); 
+    }
+    void unite(int i, int j) { 
+        int r_i=find(i), r_j=find(j); 
+        if(r_i != r_j) parent[r_i] = r_j; 
     }
 };
 
-void kruskalMST(Graph& g) {
-    vector<Edge> mst_edges;
-    long long total_mst_weight = 0;
-    
-    sort(g.edges.begin(), g.edges.end());
+class Graph {
+public:
+    int V;
+    vector<Edge> edges;
 
-    DSU dsu(g.V);
+    Graph(int vertices) : V(vertices) {}
 
-    cout << "\n--- Kruskal's Algorithm Execution ---\n";
-    for (const auto& edge : g.edges) {
-        int root_src = dsu.find(edge.src);
-        int root_dest = dsu.find(edge.dest);
-
-        if (root_src != root_dest) {
-            mst_edges.push_back(edge);
-            dsu.unite(edge.src, edge.dest);
-            total_mst_weight += edge.weight;
-            cout << "Added edge: " << edge.src << " -- " << edge.dest << "\n";
-        }
+    void addEdge(int src, int dest, int weight) {
+        edges.push_back({src, dest, weight});
     }
-    cout << "-------------------------------------\n";
-    cout << "Total Cost of Minimum Spanning Tree: " << total_mst_weight << endl;
-}
+
+    void kruskalMST() 
+    {
+        std::vector<Edge> mst_edges;
+        long long total_mst_weight = 0;
+        
+        std::sort(edges.begin(), edges.end());
+        DSU dsu(V);
+        
+        std::cout << "\n--- Kruskal's Algorithm Execution ---\n";
+        std::cout << "Processing edges sorted by weight:\n";
+
+        for (const auto& edge : edges) {
+            int root_src = dsu.find(edge.src);
+            int root_dest = dsu.find(edge.dest);
+            if (root_src != root_dest) {
+                mst_edges.push_back(edge);
+                dsu.unite(edge.src, edge.dest);
+                total_mst_weight += edge.weight;
+                std::cout << "Added edge: " << edge.src << " -- " << edge.dest << " (Weight: " << edge.weight << ")\n";
+            } else {
+                std::cout << "Skipped edge (forms cycle): " << edge.src << " -- " << edge.dest << " (Weight: " << edge.weight << ")\n";
+            }
+        }
+        std::cout << "-------------------------------------\n";
+        std::cout << "Total Cost of Minimum Spanning Tree: " << total_mst_weight << std::endl;
+    }
+};
 
 int main() {
-    cout << "===== Network Design using Kruskal's Algorithm =====\n";
-    Graph network(4);
-    network.edges.push_back({0, 1, 10});
-    network.edges.push_back({1, 2, 2});
-    network.edges.push_back({0, 2, 6});
-    network.edges.push_back({2, 3, 4});
-    network.edges.push_back({0, 3, 5});
-    network.edges.push_back({1, 3, 1}); 
-
-    kruskalMST(network);
+    std::cout << "===== Network Design using Kruskal's Algorithm for MST =====\n";
+    Graph communication_network(4);
+    communication_network.addEdge(0, 1, 10);
+    communication_network.addEdge(1, 3, 1);
+    communication_network.addEdge(2, 3, 4);
+    communication_network.addEdge(0, 2, 6);
+    communication_network.addEdge(0, 3, 5);
+    communication_network.kruskalMST();
     return 0;
 }
