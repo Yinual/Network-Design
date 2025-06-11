13a1bdbc6e86b3fef858b09b76967bb47fdede62
Abrham465 <abrhamdile303@gmail.com>
2025-06-11 14:19:50 +0300

    Update mst_Kruskal's.cpp

--- mst_Kruskal's.cpp
+++ mst_Kruskal's.cpp
@@ -2,45 +2,53 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 struct Edge {
     int src, dest;
     int weight;
-
     bool operator<(const Edge& other) const {
         return weight < other.weight;
     }
 };
-
 // Represents a graph using a list of vertices and edges.
 struct Graph {
     int V;
     vector<Edge> edges;
 };
 
+struct DSU {
+    vector<int> parent;
+    
+    DSU(int n) {
+        parent.resize(n);
+        for(int i = 0; i < n; ++i) {
+            parent[i] = i; 
+        }
+    }
+
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return find(parent[i]);
+    }
+    
+};
+
+
 void kruskalMST(Graph& g) {
     sort(g.edges.begin(), g.edges.end());
-    
-    cout << "\n--- Kruskal's Algorithm Execution ---\n";
-    cout << "Edges sorted by weight:\n";
-    for(const auto& edge : g.edges) {
-        cout << "Edge " << edge.src << "-" << edge.dest << " Weight: " << edge.weight << endl;
-    }
+
+    DSU dsu(g.V);
+    cout << "\nDSU structure initialized for " << g.V << " vertices.\n";
 
 }
 
 int main() {
-    cout << "===== Kruskal's Algorithm - Sorting Step =====\n";
-    
+    cout << "===== Kruskal's Algorithm - Adding DSU =====\n";
     Graph graph;
     graph.V = 4;
     graph.edges.push_back({0, 1, 10});
-    graph.edges.push_back({1, 2, 2}); 
+    graph.edges.push_back({1, 2, 2});
     graph.edges.push_back({0, 2, 6});
-    graph.edges.push_back({2, 3, 4});
-    graph.edges.push_back({0, 3, 5});
-
     kruskalMST(graph);
-    
     return 0;
 }
