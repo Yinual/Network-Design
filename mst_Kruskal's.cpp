2999576d9a701499505731efd99fa6e06a6489e9
Yinual <yinual3@gmail.com>
2025-06-11 14:14:14 +0300

    Update mst_Kruskal's.cpp

--- mst_Kruskal's.cpp
+++ mst_Kruskal's.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+struct Edge
+{
+    int src,dest;
+    int weight;
+};
+
+struct Graph {
+    int v;
+    vector<Edge> edges; // list of edges
+};
+
+void findMST (Graph& g){
+    cout <<"\nfindMST function called, but not yet implemented."<< endl;
+}
+
 int main(){
-cout <<"Kruskal's Algorithm for MST - Project Initialized"<< endl;
-return 0;
+    cout<< "kruskal's Algorithm for mst defining structures"<<endl;
+    Graph graph;
+    graph.v=4;
+
+    graph.edges.push_back({0,1,10});
+    graph.edges.push_back({0,2,6});
+    graph.edges.push_back({0,3,5});
+
+    cout<< "Graph created with " << graph.v<<" vertices and " <<graph.edges.size()<<" edges."<<endl;
+    findMST(graph);
+    return 0;
 }
